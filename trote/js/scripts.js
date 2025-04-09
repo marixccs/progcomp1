6b4b96e35fdfd7669a2529cb168beafe5fb63131
Mariana Soares <marianaccsoares@outlook.com>
2025-04-08 22:11:53 -0300

    alteração site trote

--- trote/js/scripts.js
+++ trote/js/scripts.js
@@ -1,7 +1,7 @@
 function calcular(){
     // vamos criar 2 variáveis
     // JS as variáveis não possuem tipo (ex.: inteiro, real, cadeira etc)
-    let mascote, homenagem, leite, kit, suplemento, pacoteArroz5kg, pacoteArroz1kg, pacoteFeijao2kg, pacoteFeijao1kg, pacoteMacarrao, oleo, soma, equipe
+    let mascote, homenagem, leite, kit, suplemento, pacoteArroz5kg, pacoteArroz1kg, pacoteFeijao2kg, pacoteFeijao1kg, pacoteMacarrao, oleo, sangue, soma, equipe
     // recupera o VALOR do mascote digitado pelo usuário
     mascote = Number(document.getElementById("mascote").value)
     // recupera o VALOR da homenagem digitado pelo usuário
@@ -18,26 +18,82 @@ function calcular(){
     pacoteFeijao1kg = Number(document.getElementById("pacoteFeijao1kg").value)
     pacoteMacarrao = Number(document.getElementById("pacoteMacarrao").value)
     oleo = Number(document.getElementById("oleo").value)
+    sangue = Number(document.getElementById("sangue").value)
     // calcular a soma, o valor chega como texto, então tem que converter o conteúdo da variável em número 
     // calcular soma parcial 
-    soma = mascote + homenagem + (2 * leite) + (30 * kit) + (15 * suplemento) + (5 * pacoteArroz5kg) + pacoteArroz1kg + (2 * pacoteFeijao2kg) + pacoteFeijao1kg + (pacoteMacarrao/2) + oleo
+    soma = mascote + homenagem + (2 * leite) 
     // vamos calcular a pontuacao considerando metas de kit e suplementos
     equipe = document.getElementById("equipe").value
     if(equipe == "Laranja"){
-        if (kit >= 97){
-            soma = soma + 5000 + ((kit-97)*30)
+        // verifica kit e suplemento
+        if (kit >= 97 && suplemento >= 49){
+            soma = soma + 5000 + ((kit-97)*30) + ((suplemento - 49) * 15) 
         }
-        else if (kit >= 78){
-            soma = soma + 4000 + ((kit-78)*30)
-        }
+        else if (kit >= 78 && suplemento >= 39){
+            soma = soma + 4000 + ((kit-78)*30) + ((suplemento - 39) * 15)
+        } 
         else if (kit >= 40){
-            soma = soma + 2500 + ((kit-40)*30)
+            soma = soma + 2500 + ((kit-40)*30) + ((suplemento - 25) * 15)
         }
         else if (kit >= 19){
-            soma = soma + 1000 + ((kit-19)*30)
+            soma = soma + 1000 + ((kit-19)*30) + ((suplemento - 10) * 15)
+        }
+        // verifica sangue 
+        if (sangue >= 49){
+            soma = soma + 2500 + ((sangue - 49) * 20)
+        }
+        else{
+            soma = soma + (sangue * 20)
         }
     }
+    else if (equipe == "Preta"){
+        // verifica kit + suplemento 
+
+        // verifica sangue
+        if (sangue >= 52) {
+            soma = soma + 2500 + ((soma - 52) * 20)
+        }
+        else{
+            soma = (soma * 20)
+        }
+    }
+    else if (equipe == "Roxa"){
+        // verifica kit + suplemento 
+
+        // verifica sangue
+        if (sangue >= 51) {
+            soma = soma + 2500 + ((soma - 51) * 20)
+        }
+        else{
+            soma = (soma * 20)
+        }
+    }
+    else if (equipe == "Verde"){
+        // verifica kit + suplemento 
+
+        // verifica sangue
+        if (sangue >= 44) {
+            soma = soma + 2500 + ((soma - 44) * 20)
+        }
+        else{
+            soma = (soma * 20)
+        }
+
+    }
+    else {  //equipe vermelha
+        // verifica kit + suplemento 
+
+        // verifica sangue
+        if (sangue >= 47) {
+            soma = soma + 2500 + ((soma - 47) * 20)
+        }
+        else{
+            soma = (soma * 20)
+        }
+    }
+
+
     // retorna o valor ao HTML pega a soma e joga para o html  
     // template string - se usa CRASE (virada para a direita)
-    document.getElementById("soma").innerHTML = `A soma é ${soma}`   
+    document.getElementById("soma").innerHTML = `A soma é ${soma}`  
 }
